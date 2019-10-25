4b9e46da435e97f236a98d67d6d0d679285ff21a
Eric Posas <web_dev@tutanota.com>
2019-10-25 09:06:02 -0500

    improved login check; need to set a more unique localStore name

--- react-frontend/src/components/Pages/TestProtectedRoute.js
+++ react-frontend/src/components/Pages/TestProtectedRoute.js
@@ -1,30 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect, history } from 'react-router'
 import { withRouter } from 'react-router-dom'
-import { getUser, checkAuth } from '../../localStore'
+// import { getUser, checkAuth } from '../../localStore'
 
 const TestProtectedRoute = ({ ...props }) => {
 
-  const routeRedirect = () => {
-    console.log('user is not authenticated!')
-    setTimeout(() => { props.history.push('/') }, 3000)
-    return (
-      <>
-        <div> Hey! You're not supposed to be in here!! >:( </div>
-      </>
-    )
+  const [appUser, setAppUser] = useState()
+
+  const [isLoggedIn, setIsLoggedIn] = useState()
+
+  // const routeRedirect = () => {
+  //   console.log('user is not authenticated!')
+  //   setTimeout(() => { props.history.push('/') }, 3000)
+  //   return (
+  //     <>
+  //       <div> Hey! You're not supposed to be in here!! >:( </div>
+  //     </>
+  //   )
+  // }
+
+  const checkLogin = () => {
+    let userData = JSON.parse(localStorage.getItem('appUser'))
+    if (userData) {
+      setIsLoggedIn(true)
+      setAppUser(userData.data.user.username)
+    } else {
+      setIsLoggedIn(false)
+      setTimeout(() => props.history.push('/'), 2000)
+    }
   }
 
-  if (checkAuth()) {
-    let user = getUser().user.username
-    console.log('user is authenticated, you are free to continue.')
+  useEffect(() => checkLogin(), [])
+
+  // useEffect(() => {
+  //   checkLogin()
+  //   // if (!isLoggedIn) { setIsLoggedIn(false) }
+  //   // checkLogin()
+  // }, [isLoggedIn])
+
+  if (isLoggedIn) {
     return (
       <>
-        <div>Here is a protected test route, "{user}" -- You're only able to see this because you are logged in as "{user}"</div>
+        <div>Here is a protected test route, "{appUser}" -- You're only able to see this because you are logged in as "{appUser}"</div>
       </>
     )
   } else {
-    return routeRedirect()
+    // setTimeout(() => props.history.push('/'))
+    return (
+      <>
+        <div>This is a protected route, you will be redirected now...</div>
+      </>
+    )
   }
 
 }
