c35702b752382c545bd3ec6b5d051de35ae16161
Eric Posas <web_dev@tutanota.com>
2019-10-24 23:36:30 -0500

    reset to localStore version

---
+++ strapi-backend/api/airport/controllers/Session.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  cookie: async ctx => {
+    console.log(ctx.session)
+
+    ctx.response.set('Set-Cookie', cookie.serialize('beep', String('boop'), {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7 // 1 week
+    }))
+    console.log(ctx.response.header)
+
+  },
+
+  session: async ctx => {
+
+    console.log(ctx.body)
+    // ctx.session.jwt
+
+  }
+
+}
