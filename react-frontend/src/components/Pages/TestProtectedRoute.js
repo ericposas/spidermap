f37bba9263390d44e6f5c97d9e6fecc20dbe8383
Eric Posas <web_dev@tutanota.com>
2019-10-25 12:01:31 -0500

    removed commented code

--- react-frontend/src/components/Pages/TestProtectedRoute.js
+++ react-frontend/src/components/Pages/TestProtectedRoute.js
@@ -1,23 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Redirect, history } from 'react-router'
 import { withRouter } from 'react-router-dom'
-// import { getUser, checkAuth } from '../../localStore'
 
 const TestProtectedRoute = ({ ...props }) => {
 
   const [appUser, setAppUser] = useState()
 
   const [isLoggedIn, setIsLoggedIn] = useState()
-
-  // const routeRedirect = () => {
-  //   console.log('user is not authenticated!')
-  //   setTimeout(() => { props.history.push('/') }, 3000)
-  //   return (
-  //     <>
-  //       <div> Hey! You're not supposed to be in here!! >:( </div>
-  //     </>
-  //   )
-  // }
 
   const checkLogin = () => {
     let userData = JSON.parse(localStorage.getItem(process.env.APP_NAME))
@@ -32,12 +21,6 @@ const TestProtectedRoute = ({ ...props }) => {
 
   useEffect(() => checkLogin(), [])
 
-  // useEffect(() => {
-  //   checkLogin()
-  //   // if (!isLoggedIn) { setIsLoggedIn(false) }
-  //   // checkLogin()
-  // }, [isLoggedIn])
-
   if (isLoggedIn) {
     return (
       <>
@@ -45,7 +28,6 @@ const TestProtectedRoute = ({ ...props }) => {
       </>
     )
   } else {
-    // setTimeout(() => props.history.push('/'))
     return (
       <>
         <div>This is a protected route, you will be redirected now...</div>
