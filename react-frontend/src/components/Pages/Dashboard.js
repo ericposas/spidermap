25ed6c1823a454396a8939bbb899291dd2d0d515
Eric Posas <web_dev@tutanota.com>
2019-10-26 09:50:43 -0500

    added basic styles to buttons and modals

--- react-frontend/src/components/Pages/Dashboard.js
+++ react-frontend/src/components/Pages/Dashboard.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import { withRouter } from 'react-router-dom'
 
 const Dashboard = ({ ...props }) => {
 
-  const { logoutHandler, user } = props
+  const { logoutHandler, user, history } = props
 
-
-
+  const airportsPageButtonHandler = () => history.push('/getAirports')
 
   return (
     <>
       <div>Welcome {user}</div>
-      <button style={{float:'right'}} onClick={logoutHandler}>logout</button>
+      <div>See airports list</div>
+      <button className='button-plain' onClick={airportsPageButtonHandler}>Airports</button>
+      <button className='button-logout' onClick={logoutHandler}>logout</button>
     </>
   )
 
 }
 
-export default Dashboard
+export default withRouter(Dashboard)
