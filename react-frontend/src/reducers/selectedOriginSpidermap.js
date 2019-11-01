40180329e790793356afa4556e4cbb823ea61fef
Eric Posas <web_dev@tutanota.com>
2019-11-01 16:19:18 -0500

    we can now delete the origin from the Spidermap view

--- react-frontend/src/reducers/selectedOriginSpidermap.js
+++ react-frontend/src/reducers/selectedOriginSpidermap.js
@@ -1,13 +1,12 @@
 import { SET_ORIGIN_SPIDERMAP, REMOVE_AN_ORIGIN_SPIDERMAP } from '../constants/constants'
 
-const selectedOriginSpidermap = (state = [], action) => {
+const selectedOriginSpidermap = (state = null, action) => {
   switch (action.type) {
     case SET_ORIGIN_SPIDERMAP:
       return action.payload
       break;
     case REMOVE_AN_ORIGIN_SPIDERMAP:
-      state = []
-      return state
+      return null
       break;
     default:
       return state
--- react-frontend/src/url.js
+++ react-frontend/src/url.js
@@ -1,4 +1,5 @@
-const url = (process.env.MODE == 'development' ? `http://${process.env.DEV_IP}` : `http://${process.env.PROD_IP}`)
+const url = `http://${process.env.DEV_IP}`
+// const url = (process.env.MODE == 'development' ? `http://${process.env.DEV_IP}` : `http://${process.env.PROD_IP}`)
 // const url = `http://${process.env.DEV_IP}:${process.env.PORT}`
 
 export default url
