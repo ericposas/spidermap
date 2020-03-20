d139c7b32c92b7ea6bd673532de05e1d881d8077
Ubuntu <ubuntu@ip-172-26-2-176.us-east-2.compute.internal>
2020-03-20 01:48:53 +0000

    added pm2 restart script

---
+++ strapi-backend/ecosystem.config.js
@@ -0,0 +1,18 @@
+   module.exports = {
+     apps: [
+       {
+         name: 'spidermap',
+         script: 'strapi start',
+         instances: 1,
+         autorestart: true,
+         watch: false,
+         max_memory_restart: '1G',
+         env: {
+           NODE_ENV: 'development',
+         },
+         env_production: {
+           NODE_ENV: 'production',
+         },
+       },
+     ],
+   };
